51837589e47169d6ba172f8a10d66c2bf9ea74c8
usu2@ Name <jesus@example.com>
2022-02-12 00:02:37 +0100

    cambio1

---
+++ prueba1/src/Cambio1/cambio1.java
@@ -0,0 +1,13 @@
+package Cambio1;
+
+public class cambio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pruebas Git. Clase cambio1");
+		
+	}
+	
+
+}
+	
